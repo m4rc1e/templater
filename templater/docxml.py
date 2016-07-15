dcc33d2237599c24deb5f7def8d838888330b5ed
Marc Foley <m.foley.88@gmail.com>
2016-07-15 11:43:58 +0200

    [templater.docxml] Docxml is no independent of any library/framework. The docx file is now loaded and manipulated directly inside the Document class

---
+++ templater/docxml.py
@@ -0,0 +1,28 @@
+from lxml import etree
+import shutil
+import os
+from zipfile import ZipFile
+import re
+from os.path import basename
+
+
+class Document:
+    '''Primitive docx parser.'''
+
+    def __init__(self, file_path=None):
+        with ZipFile(file_path) as self._docx:
+            self._files = {name: self._docx.read(name) for name in self._docx.namelist()}
+
+    def replace_text(self, current, new):
+        text = self._files['word/document.xml'].decode('utf-8')
+        text = re.sub(current, new, text)
+        self._files['word/document.xml'] = bytes(text, 'utf-8')
+
+    def save(self, path):
+        with ZipFile(path, 'w') as doc:
+            for file in self._files:
+                doc.writestr(file, self._files[file])
+
+    @property
+    def files(self):
+        return self._files
